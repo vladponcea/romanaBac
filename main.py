201e217ebfff04a1206275d5486166e66b9f8c82
vladponcea <vladponcea@icloud.com>
2022-05-27 18:21:29 +0300

    culori + counter corecte, gresite

--- main.py
+++ main.py
@@ -13,16 +13,30 @@ test = int(input("\nTipul de test: "))
 nr = int(input("Numarul de teste: "))
 
 if test == 1:
+    corecte = 0
+    gresite = 0
+    opereVerificate = []
     while nr:
         opera = random.choice(data["opere"])
+        while opera in opereVerificate:
+            opera = random.choice(data["opere"])
+        opereVerificate.append(opera)
+        
         an = int(opera["an"])
-        culori.puts(opera["numeTitlu"] + culori.colors.blue)
+        culori.puts("\n" + str(opera["numeTitlu"]), culori.colors.blue)
         culori.puts("\nAnul aparitiei este: ", culori.colors.white)
         userAn = int(input())
+        
         if an == userAn:
+            corecte += 1
             culori.puts("\nRASPUNS CORECT", culori.colors.green) 
         else:
+            gresite += 1
             culori.puts("\nRASPUNS GRESIT. MAI INVATA...", culori.colors.red)
+        
         print("\nRaspunsul corect: " + str(an))
         print("Raspunusl tau: " + str(userAn))
         nr -= 1
+
+    culori.puts("\nTotal raspunsuri corecte: " + str(corecte), culori.colors.green)
+    culori.puts("\nTotal raspunsuri gresite: " + str(gresite), culori.colors.red)

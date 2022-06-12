65099408cd002d85abf31be6346395077aa29d26
vladponcea <vladponcea@icloud.com>
2022-06-12 20:29:51 +0300

    imbunatatiri raspunsuri

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ import culori
 file = open('opere.json')
 data = json.load(file)
 
-print("1. Ani\n2. Tema")
-test = int(input("\nTipul de test: "))
-nrOpere = len(data["opere"])
-nr = int(input("Numarul de teste(maxim " + str(nrOpere) + " teste): "))
-
-if test == 1:
+def ani(nrTeste):
     corecte = 0
     gresite = 0
     opereVerificate = []
-    while nr:
+    while nrTeste:
         opera = random.choice(data["opere"])
         while opera in opereVerificate:
             opera = random.choice(data["opere"])
@@ -37,13 +32,16 @@ if test == 1:
         
         print("\nRaspunsul corect: " + str(an))
         print("Raspunusl tau: " + str(userAn))
-        nr -= 1
-elif test == 2:
+        nrTeste -= 1
+    
+    return corecte, gresite
+
+def tema(nrTeste):
     culori.puts("\nDeoarece ai ales acest tip de test, verificarea va trebui facuta de tine\n", culori.colors.red)
     corecte = 0
     gresite = 0
     opereVerificate = []
-    while nr:
+    while nrTeste:
         opera = random.choice(data["opere"])
         while opera in opereVerificate:
             opera = random.choice(data["opere"])
@@ -58,13 +56,47 @@ elif test == 2:
         for x in tema:
             culori.puts(str(x) + '\n', culori.colors.green)
         print("Raspunusl tau: " + userTema)
-        nr -= 1
+        nrTeste -= 1
 
         tip = int(input("CORECT(1), GRESIT(2): "))
         if tip == 1:
             corecte += 1
         else:
             gresite += 1
+    
+    return corecte, gresite
 
-culori.puts("\nTotal raspunsuri corecte: " + str(corecte), culori.colors.green)
-culori.puts("\nTotal raspunsuri gresite: " + str(gresite) + '\n', culori.colors.red)
+if __name__ == "__main__":
+    iesire = False
+
+    totalCorecte = 0
+    totalGresite = 0
+
+    while not iesire:
+        print("1. Ani\n2. Tema")
+        test = int(input("\nTipul de test: "))
+        nrOpere = len(data["opere"])
+        nr = int(input("Numarul de teste(maxim " + str(nrOpere) + " teste): "))
+        if test == 1:
+            corecteTest, gresiteTest = ani(nr)
+            totalCorecte += corecteTest
+            totalGresite += gresiteTest
+        elif test == 2:
+            corecteTest, gresiteTest = ani(nr)
+            totalCorecte += corecteTest
+            totalGresite += gresiteTest
+
+        culori.puts("\nRapspunsuri corecte pentru acest test: " + str(corecteTest), culori.colors.green)
+        culori.puts("\nRaspunsuri gresite pentru acest test: " + str(gresiteTest) + '\n', culori.colors.red)
+
+        culori.puts("\nVrei sa continui cu alt test?(DA/NU)\n", culori.colors.cyan)
+        continua = input()
+        os.system('clear')
+
+        if continua.upper() == "NU":
+            iesire = True
+
+        
+
+    culori.puts("\nTotal raspunsuri corecte: " + str(totalCorecte), culori.colors.green)
+    culori.puts("\nTotal raspunsuri gresite: " + str(totalGresite) + '\n', culori.colors.red)
